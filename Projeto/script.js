6527c3df66bb66f7da86f195b85286cfc9cc460c
Pedro <102542541+PedroR-19@users.noreply.github.com>
2023-06-14 13:50:29 -0300

    Add files via upload

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,36 +1,23 @@
 const apiKey = "c178c19843cf8020f701f1a4";
-const Button = document.getElementById("button");
-const Value = document.getElementById("value");
 
-async function getQuote(baseCurrency, targetCurrency) {
-    try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/pair/${baseCurrency}/${targetCurrency}`);
-      const data = await response.json();
-      const exchangeRate = data.conversion_rate;
-      
-      Value.textContent = `1 ${baseCurrency} = ${exchangeRate} ${targetCurrency}`;
+async function converter(targetCurrency) {
+  const valorReal = document.getElementById("valor").value;
+  const response = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/pair/BRL/${targetCurrency}`);
+  const data = await response.json();
+  const exchangeRate = data.conversion_rate;
   
-      // Definir a imagem e a nota da currency 1
-      const currency1Img = document.getElementById("currency-1-img");
-      const currency1Note = document.getElementById("currency-1-note");
-      currency1Img.src = `imagens/${baseCurrency}.jpg`;
-      currency1Img.alt = baseCurrency;
-      currency1Note.textContent = `Nota: ${baseCurrency}`;
-  
-      // Definir a imagem e a nota da currency 2
-      const currency2Img = document.getElementById("currency-2-img");
-      const currency2Note = document.getElementById("currency-2-note");
-      currency2Img.src = `imagens/${targetCurrency}.jpg`;
-      currency2Img.alt = targetCurrency;
-      currency2Note.textContent = `Nota: ${targetCurrency}`;
-    } catch (error) {
-      console.log("Ocorreu um erro ao obter a cotação:", error);
-    }
+  const convertedValue = valorReal * exchangeRate;
+
+  // Limpar os elementos antes de atualizar os valores
+  document.getElementById("dolar").textContent = "";
+  document.getElementById("euro").textContent = "";
+  document.getElementById("yens").textContent = "";
+
+  if (targetCurrency === "USD") {
+    document.getElementById("dolar").textContent = `Valor em Dólar: $${convertedValue.toFixed(2)}`;
+  } else if (targetCurrency === "EUR") {
+    document.getElementById("euro").textContent = `Valor em Euro: €${convertedValue.toFixed(2)}`;
+  } else if (targetCurrency === "JPY") {
+    document.getElementById("yens").textContent = `Valor em Yens: ¥${convertedValue.toFixed(2)}`;
   }
-
-Button.addEventListener("click", function() {
-  const currency1 = document.getElementById("currency-1").value;
-  const currency2 = document.getElementById("currency-2").value;
-
-  getQuote(currency1, currency2);
-});
+}
